2a9af3eb383b655f0b3149fb020f551080c646ca
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2026-07-20 23:26:01 -0400

    Remove tests

--- eslint.config.js
+++ eslint.config.js
@@ -4,10 +4,8 @@ import path from 'node:path';
 import pluginJs from '@eslint/js';
 import tjwBase from 'eslint-config-tjw-base';
 import tjwImport from 'eslint-config-tjw-import-x';
-import tjwJest from 'eslint-config-tjw-jest';
 import tjwVue from 'eslint-config-tjw-vue';
 import pluginImport from 'eslint-plugin-import-x';
-import pluginJest from 'eslint-plugin-jest';
 import pluginVue from 'eslint-plugin-vue';
 import pluginVueA11y from 'eslint-plugin-vuejs-accessibility';
 
@@ -18,12 +16,10 @@ const vueA11yRecommended = pluginVueA11y.configs['flat/recommended'];
 const config = [
   pluginJs.configs.recommended,
   pluginImport.flatConfigs.recommended,
-  pluginJest.configs['flat/recommended'],
   ...vue3Recommended,
   ...vueA11yRecommended,
   tjwBase.configs.recommended,
   tjwImport,
-  tjwJest.configs.recommended,
   tjwVue,
   {
     languageOptions: {
@@ -35,8 +31,6 @@ const config = [
     // project specific rules/settings
     rules: {
       'import-x/no-unresolved': 'off',
-      // If this is not turned off, linting throws because it can't find 'jest' install
-      'jest/no-deprecated-functions': 'off',
       'vuejs-accessibility/label-has-for': [
         'error',
         {
@@ -54,8 +48,7 @@ const config = [
           viteConfig: {
             resolve: {
               alias: {
-                '@': path.resolve(__dirname, 'src'),
-                '@@': path.resolve(__dirname, 'tests')
+                '@': path.resolve(__dirname, 'src')
               }
             }
           }
--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,6 @@ import vue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vite';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import vueDevTools from 'vite-plugin-vue-devtools';
-/* eslint-disable-next-line import-x/extensions */
-import { configDefaults } from 'vitest/config';
 
 const __dirname = import.meta.dirname;
 
@@ -24,37 +22,10 @@ const config = defineConfig({
     }),
     nodePolyfills()
   ],
-  optimizeDeps: {
-    exclude: ['vue-dev-tools-accessibility']
-  },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      '@@': fileURLToPath(new URL('./tests', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
-  test: {
-    coverage: {
-      exclude: [
-        ...(configDefaults?.coverage?.exclude || []),
-        '.eslintrc.cjs',
-        './src/main.js',
-        '**/scripts/',
-        '**/tests/'
-      ],
-      reportsDirectory: './tests/unit/coverage'
-    },
-    environment: 'happy-dom',
-    // https://github.com/capricorn86/happy-dom/issues/1950
-    execArgv: ['--no-experimental-webstorage'],
-    globals: true,
-    root: '.',
-    setupFiles: [
-      './tests/unit/setup.js'
-    ],
-    snapshotSerializers: [
-      './node_modules/vue3-snapshot-serializer/index.js'
-    ]
   }
 });
 
