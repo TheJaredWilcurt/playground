a2a0d6833b3474b6040d6c6845f9e05777bd511a
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2026-09-24 07:18:25 -0500

    deps, node

--- scripts/pre/start.js
+++ scripts/pre/start.js
@@ -2,8 +2,8 @@
  * @file Automatically ran when just before when `npm start` is ran.
  */
 
-import { copySortOrder } from './copySortOrder.js';
-import { copyWasm } from './copyWasm.js';
+import { copySortOrder } from '../copySortOrder.js';
+import { copyWasm } from '../copyWasm.js';
 
 copySortOrder();
 copyWasm();
